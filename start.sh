8019c8102789579da67711435b996aeac6f80cef
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-06-09 11:35:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/ankitkhandal74/Google-Bard-Bot.git /Google-Bard-Bot
-else
- echo "Cloning Custom Repo from $UPSTREAM_REPO "
- git clone $UPSTREAM_REPO /Google-Bard-Bot
-fi
+#if [ -z $UPSTREAM_REPO ]
+#then
+#  echo "Cloning main Repository"
+#  git clone https://github.com/ankitkhandal74/Google-Bard-Bot.git /Google-Bard-Bot
+#else
+# echo "Cloning Custom Repo from $UPSTREAM_REPO "
+ #git clone $UPSTREAM_REPO /Google-Bard-Bot
+#fi
 #cd /Google-Bard-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Google bard"
