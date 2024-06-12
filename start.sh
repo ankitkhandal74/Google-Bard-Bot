491be86e19fd391cc48cfe8f8635daaa26224bae
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-06-12 12:08:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
+  rm -rf /Google-Bard-Bot  # Remove the directory if it exists
   git clone https://github.com/ankitkhandal74/Google-Bard-Bot.git /Google-Bard-Bot
 else
  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+ rm -rf /Google-Bard-Bot  # Remove the directory if it exists
  git clone $UPSTREAM_REPO /Google-Bard-Bot
 fi
 cd /Google-Bard-Bot
