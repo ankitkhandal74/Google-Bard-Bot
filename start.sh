d084b9aa07e5b7be8ac6298499d76e3ba90cec37
Ankit khandal <160460793+ankitkhandal74@users.noreply.github.com>
2024-06-12 12:31:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ankitkhandal74/-yuji-itadori.git /-yuji-itadori
+  git clone https://github.com/ankitkhandal74/Google-Bard-Bot.git /Google-Bard-Bot
 else
  echo "Cloning Custom Repo from $UPSTREAM_REPO "
- git clone $UPSTREAM_REPO /-yuji-itadori
+ git clone $UPSTREAM_REPO /Google-Bard-Bot
 fi
-cd /-yuji-itadori
-#pip3 install -U -r requirements.txt
+cd /Google-Bard-Bot
+pip3 install -U -r requirements.txt
 echo "Starting -yuji-itadori"
 python3 bot.py
